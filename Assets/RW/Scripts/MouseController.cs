2b20e9a3e6335db0c9c1f2e52f97b33f95ec10ee
Hải Vân <k34ctqer@gmail.com>
2025-11-04 22:46:23 +0700

    done part 1.2

---
+++ Assets/RW/Scripts/MouseController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseController : MonoBehaviour
+{
+    public float jetpackForce = 75.0f;
+    private Rigidbody2D playerRigidbody;
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Mouse.current.leftButton.isPressed)
+        {
+            playerRigidbody.AddForce(new Vector2(0, jetpackForce));
+        }
+    }
+}
