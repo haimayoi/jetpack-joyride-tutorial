4638b5ab81e029eac000866dc16e8d19b4bb1fe9
Hải Vân <k34ctqer@gmail.com>
2025-11-05 22:00:50 +0700

    done part 3.5

--- Assets/RW/Scripts/MouseController.cs
+++ Assets/RW/Scripts/MouseController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MouseController : MonoBehaviour
 {
@@ -15,6 +16,10 @@ public class MouseController : MonoBehaviour
     private bool isDead = false;
     private uint coins = 0;
     public Text coinsCollectedLabel;
+    public Button restartButton;
+    public AudioClip coinCollectSound;
+    public AudioSource jetpackAudio;
+    public AudioSource footstepsAudio;
 
     void Start()
     {
@@ -39,8 +44,14 @@ public class MouseController : MonoBehaviour
             playerRigidbody.velocity = newVelocity;
         }
 
+        if (isDead && isGrounded)
+        {
+            restartButton.gameObject.SetActive(true);
+        }
+
         UpdateGroundedStatus();
         AdjustJetpack(jetpackActive);
+        AdjustFootstepsAndJetpackSound(jetpackActive);
     }
 
     void UpdateGroundedStatus()
@@ -79,6 +90,11 @@ public class MouseController : MonoBehaviour
 
     void HitByLaser(Collider2D laserCollider)
     {
+        if (!isDead)
+        {
+            AudioSource laserZap = laserCollider.gameObject.GetComponent<AudioSource>();
+            laserZap.Play();
+        }
         isDead = true;
         mouseAnimator.SetBool("isDead", true);
     }
@@ -88,5 +104,26 @@ public class MouseController : MonoBehaviour
         coins++;
         coinsCollectedLabel.text = coins.ToString();
         Destroy(coinCollider.gameObject);
+        AudioSource.PlayClipAtPoint(coinCollectSound, transform.position);
     }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("RocketMouse");
+    }
+
+    void AdjustFootstepsAndJetpackSound(bool jetpackActive)
+    {
+        footstepsAudio.enabled = !isDead && isGrounded;
+        jetpackAudio.enabled = !isDead && !isGrounded;
+        if (jetpackActive)
+        {
+            jetpackAudio.volume = 1.0f;
+        }
+        else
+        {
+            jetpackAudio.volume = 0.5f;
+        }
+    }
+
 }
