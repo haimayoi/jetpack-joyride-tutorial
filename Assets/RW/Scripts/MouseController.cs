3fd8d4339db71290897463a309e0880c3b133b94
Hải Vân <k34ctqer@gmail.com>
2025-11-05 14:24:50 +0700

    done part 2

--- Assets/RW/Scripts/MouseController.cs
+++ Assets/RW/Scripts/MouseController.cs
@@ -5,22 +5,56 @@ public class MouseController : MonoBehaviour
 {
     public float jetpackForce = 75.0f;
     public float forwardMovementSpeed = 3.0f;
-
+    public Transform groundCheckTransform;
+    private bool isGrounded;
+    public LayerMask groundCheckLayerMask;
+    private Animator mouseAnimator;
     private Rigidbody2D playerRigidbody;
+    public ParticleSystem jetpack;
 
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        mouseAnimator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
-        if (Mouse.current.leftButton.isPressed)
+        bool jetpackActive = Mouse.current.leftButton.isPressed;
+
+        if (jetpackActive)
         {
             playerRigidbody.AddForce(new Vector2(0, jetpackForce));
         }
+
         Vector2 newVelocity = playerRigidbody.velocity;
         newVelocity.x = forwardMovementSpeed;
         playerRigidbody.velocity = newVelocity;
+
+        UpdateGroundedStatus();
+
+        AdjustJetpack(jetpackActive);
+    }
+
+    void UpdateGroundedStatus()
+    {
+        //1
+        isGrounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+        //2
+        mouseAnimator.SetBool("isGrounded", isGrounded);
+    }
+
+    void AdjustJetpack(bool jetpackActive)
+    {
+        var jetpackEmission = jetpack.emission;
+        jetpackEmission.enabled = !isGrounded;
+        if (jetpackActive)
+        {
+            jetpackEmission.rateOverTime = 300.0f;
+        }
+        else
+        {
+            jetpackEmission.rateOverTime = 75.0f;
+        }
     }
 }
