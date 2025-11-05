c781c785c7b9a8044c70b91d0b76aae1422beeea
Hải Vân <k34ctqer@gmail.com>
2025-11-05 09:38:47 +0700

    done part 2.1

--- Assets/RW/Scripts/MouseController.cs
+++ Assets/RW/Scripts/MouseController.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class MouseController : MonoBehaviour
 {
     public float jetpackForce = 75.0f;
+    public float forwardMovementSpeed = 3.0f;
+
     private Rigidbody2D playerRigidbody;
 
     void Start()
@@ -17,5 +19,8 @@ public class MouseController : MonoBehaviour
         {
             playerRigidbody.AddForce(new Vector2(0, jetpackForce));
         }
+        Vector2 newVelocity = playerRigidbody.velocity;
+        newVelocity.x = forwardMovementSpeed;
+        playerRigidbody.velocity = newVelocity;
     }
 }
