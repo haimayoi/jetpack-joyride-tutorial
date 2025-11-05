8eba51be91359eae6fa1892353aac47052653f7d
Hải Vân <k34ctqer@gmail.com>
2025-11-05 13:28:04 +0700

    done part 2.3

--- Assets/RW/Scripts/GeneratorScript.cs
+++ Assets/RW/Scripts/GeneratorScript.cs
@@ -13,6 +13,7 @@ public class GeneratorScript : MonoBehaviour
     {
         float height = 2.0f * Camera.main.orthographicSize;
         screenWidthInPoints = height * Camera.main.aspect;
+        StartCoroutine(GeneratorCheck());
     }
 
     // Update is called once per frame
@@ -35,5 +36,60 @@ public class GeneratorScript : MonoBehaviour
         room.transform.position = new Vector3(roomCenter, 0, 0);
         //6
         currentRooms.Add(room);
+    }
+
+    private void GenerateRoomIfRequired()
+    {
+        //1
+        List<GameObject> roomsToRemove = new List<GameObject>();
+        //2
+        bool addRooms = true;
+        //3
+        float playerX = transform.position.x;
+        //4
+        float removeRoomX = playerX - screenWidthInPoints;
+        //5
+        float addRoomX = playerX + screenWidthInPoints;
+        //6
+        float farthestRoomEndX = 0;
+        foreach (var room in currentRooms)
+        {
+            //7
+            float roomWidth = room.transform.Find("floor").localScale.x;
+            float roomStartX = room.transform.position.x - (roomWidth * 0.5f);
+            float roomEndX = roomStartX + roomWidth;
+            //8
+            if (roomStartX > addRoomX)
+            {
+                addRooms = false;
+            }
+            //9
+            if (roomEndX < removeRoomX)
+            {
+                roomsToRemove.Add(room);
+            }
+            //10
+            farthestRoomEndX = Mathf.Max(farthestRoomEndX, roomEndX);
+        }
+        //11
+        foreach (var room in roomsToRemove)
+        {
+            currentRooms.Remove(room);
+            Destroy(room);
+        }
+        //12
+        if (addRooms)
+        {
+            AddRoom(farthestRoomEndX);
+        }
+    }
+
+    private IEnumerator GeneratorCheck()
+    {
+        while (true)
+        {
+            GenerateRoomIfRequired();
+            yield return new WaitForSeconds(0.25f);
+        }
     }
 }
